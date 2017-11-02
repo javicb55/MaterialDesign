472e22ca2d4fad99e86b60055c4999ecbeac6538
Javi <javicb55@gmail.com>
2017-11-02 10:36:40 +0100

    commit con materialDesign

---
+++ Material01/app/src/main/java/es/vcarmen/material01/Material01.java
@@ -0,0 +1,13 @@
+package es.vcarmen.material01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Material01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material01);
+    }
+}
